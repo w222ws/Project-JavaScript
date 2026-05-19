d3b1016d39f4a5ec7f58d64c98e7db61c9b92be5
w222ws <tagshsjs697@gmail.com>
2026-05-19 23:26:07 +0300

    js вступ 3

--- trainee.js
+++ trainee.js
@@ -1,10 +1,19 @@
-const topTaskWorld = [
-    { id: 1, text: 'Зрозуміти С', completed: true, priority: 'low' },
-    { id: 2, text: 'Зрозуміти СSS', completed: false, priority: 'high' },
-    { id: 3, text: 'Зрозуміти HTML', completed: false, priority: 'high' },
-    { id: 4, text: 'Зрозуміти Pascale', completed: true, priority: 'low' }
-]; // Создав змінну з камал кейс по стандарту, далі ну обьекти, ну і стьоб зробив типу ну бля, типу с і паскал
-// типу легко вивчити а ось типу хтмл і цсс тип не реально ахахах) ну і все)) щас ебашить буду фільтр)
+// const topTaskWorld = [
+//     { id: 1, text: 'Зрозуміти С', completed: true, priority: 'low' },
+//     { id: 2, text: 'Зрозуміти СSS', completed: false, priority: 'high' },
+//     { id: 3, text: 'Зрозуміти HTML', completed: false, priority: 'high' },
+//     { id: 4, text: 'Зрозуміти Pascale', completed: true, priority: 'low' }
+// ]; // Создав змінну з камал кейс по стандарту, далі ну обьекти, ну і стьоб зробив типу ну бля, типу с і паскал
+// // типу легко вивчити а ось типу хтмл і цсс тип не реально ахахах) ну і все)) щас ебашить буду фільтр)
+//
+// const titles = topTaskWorld.map(task => task.text);
+// console.log(titles); // мапчик
 
-const titles = topTaskWorld.map(task => task.text);
-console.log(titles); // мапчик
+let user = { name: "Макс", isBlocked: true };
+
+// Что нужно написать вместо вопросов, чтобы проверить, забанен ли Макс?
+if (user.isBlocked === true) {
+    console.log("Доступ запрещен! Вы забанены.");
+} else {
+    console.log("Добро пожаловать в систему!");
+}
