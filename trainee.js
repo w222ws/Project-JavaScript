0a6642ab84815b859f30482df521d28caac9c7f7
w222ws <tagshsjs697@gmail.com>
2026-05-22 21:38:35 +0300

    js fix  arrays 2

--- trainee.js
+++ trainee.js
@@ -1,24 +1,4 @@
-// const topTaskWorld = [
-//     { id: 1, text: 'Зрозуміти С', completed: true, priority: 'low' },
-//     { id: 2, text: 'Зрозуміти СSS', completed: false, priority: 'high' },
-//     { id: 3, text: 'Зрозуміти HTML', completed: false, priority: 'high' },
-//     { id: 4, text: 'Зрозуміти Pascale', completed: true, priority: 'low' }
-// ]; // Создав змінну з камал кейс по стандарту, далі ну обьекти, ну і стьоб зробив типу ну бля, типу с і паскал
-// // типу легко вивчити а ось типу хтмл і цсс тип не реально ахахах) ну і все)) щас ебашить буду фільтр)
-//
-// const titles = topTaskWorld.map(task => task.text);
-// console.log(titles); // мапчик
-
-let user = { name: "Макс", isBlocked: true };
-
-// Что нужно написать вместо вопросов, чтобы проверить, забанен ли Макс?
-// if (user.isBlocked === true) {
-//     console.log("Доступ запрещен! Вы забанены.");
-// } else {
-//     console.log("Добро пожаловать в систему!");
-// }
-//
-// let arr1 = [1, 2]; // масс1
+ // let arr1 = [1, 2]; // масс1
 // let arr2 = [3, 4]; // масс2
 // let combined = [...arr1, ...arr2]; // комбинируем массивы наши с помощью оператора спреар, он же ... россыпь старого массива делаемт
 // console.log(combined); // ну и вывод консоль
@@ -228,4 +208,3 @@ let user = { name: "Макс", isBlocked: true };
 // const salaryBig = family.filter((s) => s.salary > 2500);
 //
 // console.log(salaryBig);
-
